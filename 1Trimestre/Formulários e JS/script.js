157f4a722a32d649a2f90cd88723dd10a370424d
Vinicius Hartmann Ferreira <vinihf@gmail.com>
2025-02-27 17:54:53 -0300

    Formulários e tipos de dados

---
+++ 1Trimestre/Formulários e JS/script.js
@@ -0,0 +1,69 @@
+// Vamos iniciar imprimindo um conteúdo no console do navegador
+console.log("oi");
+
+/*Em Javascript existem 3 tipos de escopo de variável:
+ var - global
+ let - escopo local
+ const - constante
+
+No Javascript há tipos primitivos e objetos. Os tipos primitivos são imutáveis e são atribuídos por valor.
+Já os objetos são atribuídos por referência
+
+*/
+
+/* String */
+let nome = "João";
+let nome2 = 'Maria';
+let mensagem = `Olá, ${nome}`;
+console.log(mensagem);
+
+
+/* Number */
+let idade = 25;       
+let valor = 99.99;    
+let infinito = Infinity;  
+let notANumber = NaN; 
+
+/* Boolean */
+let verdadeiro  = true;
+let falso = false;
+
+
+/* undefined */
+let valorNovo;
+console.log(valorNovo);
+
+/* Null */
+let valorNulo = null;
+
+/* Entre os tipos objetos, citam-se os objetos, arrays e date */
+
+/* Objetos */
+let time = {
+    nome: "Grêmio",
+    mundiais: 2
+};
+
+console.log(time.nome);
+
+/* Arrays */
+let times = ["Grêmio","Juventude","Caxias","Internacional"];
+
+console.log(times[1]);
+
+/* Date */
+let hoje = new Date();
+console.log(hoje);
+
+/* Vamos adicionar um evento ao nosso Formulário */
+document.getElementById("formulario").addEventListener("submit", function(event) {
+    event.preventDefault();
+    const formData = new FormData(this);
+    let dados = Object.fromEntries(formData.entries());
+    if (formData.has("proprio")) {
+        console.log("O usuário marcou alguma opção de transporte proprio.");
+    }
+    if (formData.has("publico")) {
+        console.log("O usuário marcou alguma opção de transporte público.");
+    } 
+});
