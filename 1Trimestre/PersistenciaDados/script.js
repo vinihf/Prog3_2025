d32bd8dbaf9b75067513eddcbc54e541d9bb51c3
Vinicius Hartmann Ferreira <vinihf@gmail.com>
2025-04-22 17:09:42 -0300

    Persistencia

--- 1Trimestre/PersistenciaDados/script.js
+++ 1Trimestre/PersistenciaDados/script.js
@@ -1,8 +1,23 @@
-.addEventListener()
+window.addEventListener('load',function(){
+    if(localStorage.getItem("nome")){
+        this.document.getElementById("formulario").style.display='none';
+        this.document.getElementById("nome_gravado").textContent = localStorage.getItem("nome");
+        this.document.getElementById("texto").style.display='';
+    }else{
+        this.document.getElementById("formulario").style.display='';
+        this.document.getElementById("texto").style.display='none';
+    }
+})
 
 const formulario = document.getElementById("formulario")
 formulario.addEventListener("submit",function(event){
     const formData = new FormData(this);
     let dados = Object.fromEntries(formData.entries());
     localStorage.setItem("nome",dados['nome']);
+})
+
+const apagar = document.getElementById("apagar")
+apagar.addEventListener("click",function(){
+    localStorage.removeItem("nome")
+    window.location.reload() 
 })
